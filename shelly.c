c6d96a9a800398d6822c0b1a820727566f28480d
MrBarns <barnabasoluwatosin@gmail.com>
2023-12-16 14:30:50 +0100

    Debug2 task1

--- shelly.c
+++ shelly.c
@@ -19,11 +19,13 @@ char **getav(char *lineptr, char **environ)
 	(void) environ;
 
 	arg = strtok(lineptr, "\n");
-	for (ind = 0; arg[ind]; ind++)
-	{
-		if ((ind) && (arg[ind] != ' ') && (arg[ind - 1] == ' '))
-			count++;
-	}
+	/*
+	 *for (ind = 0; arg[ind]; ind++)
+	 *{
+	 *	if ((ind) && (arg[ind] != ' ') && (arg[ind - 1] == ' '))
+	 *		count++;
+	 *}
+	*/
 	argv = malloc(sizeof(char *) * (count + 2));
 	if (!argv)
 	{
@@ -35,15 +37,47 @@ char **getav(char *lineptr, char **environ)
 	/*
 	 *getadd(arg, environ);
 	 */
-	for (ind = 0; ind < 1; ind++)
+	if (arg != NULL)
 	{
-		argv[ind] = arg;
-	}
-	argv[ind] = NULL;
+		for (ind = 0; ind < 1; ind++)
+		{
+			argv[ind] = arg;
+		}
+		argv[ind] = NULL;
 
-	return (argv);
+		return (argv);
+	}
+
+	free(argv);
+	return (NULL);
 }
 
+
+/**
+ * prompt - prompts for input
+ *
+ * Return: input string
+ */
+
+char *prompt(void)
+{
+	char *lineptr = NULL;
+	ssize_t ret;
+	size_t n = 0;
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, ":) ", 3);
+	ret = getline(&lineptr, &n, stdin);
+	if (ret == -1)
+	{
+		free(lineptr);
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "\n", 1);
+		exit(EXIT_SUCCESS);
+	}
+
+	return (lineptr);
+}
 
 /**
  * main - entry function
@@ -59,42 +93,32 @@ int main(int ac, char *av[])
 {
 	pid_t child_pid;
 	int status;
-	char **argv, *lineptr = NULL;
-	ssize_t ret;
-	size_t n = 0;
+	char **argv, *lineptr;
 
 	(void) ac;
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, ":) ", 3);
-		ret = getline(&lineptr, &n, stdin);
-		if (ret == -1)
-		{
-			free(lineptr);
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			exit(EXIT_SUCCESS);
-		}
+		lineptr = prompt();
 		if (*lineptr != '\n')
 		{
 			argv = getav(lineptr, environ);
-			child_pid = fork();
-			if (child_pid == 0)
+			if (argv)
 			{
-				execve(argv[0], argv, environ);
-				perror(av[0]);
-				exit(EXIT_FAILURE);
-			} else
-			{
-				wait(&status);
-				free(lineptr);
-				free(argv);
-				n = 0;
+				child_pid = fork();
+				if (child_pid == 0)
+				{
+					execve(argv[0], argv, environ);
+					perror(av[0]);
+					exit(EXIT_FAILURE);
+				} else
+				{
+					wait(&status);
+					free(lineptr);
+					free(argv);
+				}
 			}
-		} else
-			write(STDOUT_FILENO, "\n", 1);
+		}
 	}
 	return (0);
 }
